ba67f4abac8565186e598c3ac5e2bbe8d311e15c
Dmitriy <klimov.dima@mail.ru>
2023-09-20 09:55:43 +0300

    HW_1_fixed bugs

--- Практика/Homework/HW_1/1.js
+++ Практика/Homework/HW_1/1.js
@@ -13,8 +13,6 @@
 Уточнение: пользователь всегда вводит корректное число.
 */
 
-"use strict";
-
 const tCelsius = +prompt("Введите температуру в градусах Цельсия:");
 const tFahrenheit = (9 / 5) * tCelsius + 32;
 alert(`Цельсий: ${tCelsius};\nФаренгейт: ${tFahrenheit.toFixed(1)}`);
